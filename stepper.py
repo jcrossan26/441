e1a8cc4ee20419045e08bc7bf46dbd6dce3fdd94
James Crossan <78496463+jcrossan26@users.noreply.github.com>
2021-11-05 07:42:26 +0000

    a

--- stepper.py
+++ stepper.py
@@ -8,6 +8,16 @@ pins = [18,21,22,23] # controller inputs: in1, in2, in3, in4
 for pin in pins:
   GPIO.setup(pin, GPIO.OUT, initial=0)
 
+# Define the pin sequence for counter-clockwise motion, noting that
+# two adjacent phases must be actuated together before stepping to
+# a new phase so that the rotor is pulled in the right direction:
+ccw = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
+      [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+# Make a copy of the ccw sequence. This is needed since simply
+# saying cw = ccw would point both variables to the same list object:
+cw = ccw[:]  # use slicing to copy list (could also use ccw.copy() in Python 3)
+cw.reverse() # reverse the new cw sequence
+
 
 class zero:
   lightValueOld = 100
